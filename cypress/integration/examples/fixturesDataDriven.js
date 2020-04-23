801ce9bbc03a37c9114e6ffa0da58d4e292b824b
valdrich <polianevaldrich@gmail.com>
2020-04-23 15:59:03 -0600

    Fixtures Data Driven example

---
+++ cypress/integration/examples/fixturesDataDriven.js
@@ -0,0 +1,21 @@
+///<reference types="Cypress" />
+
+describe('How to use Fixtures to control Data Driven', function () {
+
+    before(() => {
+        cy.fixture('eCommerceData')
+            .then((data) => {
+                this.data = data
+            })
+        cy.visit('https://rahulshettyacademy.com/angularpractice/')
+    })
+
+    it('Should input form data using Fixtures', () => {
+        cy.get(':nth-child(1) > .form-control').type(this.data.name).should('have.value', this.data.name)
+        cy.get('#exampleFormControlSelect1').select(this.data.gender).should('have.value',this.data.gender)
+        cy.get('input[name="name"]:nth-child(2)').should('have.attr','minlength','2')
+        cy.get('#inlineRadio3').should('be.disabled')
+    })
+
+
+})
